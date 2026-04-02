a398b90299e991cfdab31f30f9f0f02c6ad74c60
faiz_mountblue <faiz.ahmad.31.1@mountblue.tech>
2026-04-02 11:40:10 +0530

    Converted into react code*2

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,6 +1,6 @@
 import Card from "../Card/Card";
 
-export default function Section({ section }: any) {
+export default function Section({ section }:any) {
   return (
     <div className="mt-8">
       <h2 className="text-lg font-medium text-gray-800 mb-3 pl-2 border-l-4 border-blue-500">
